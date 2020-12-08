3b92110878bb4af0cbcb64e93f82c94062afdd0e
rostyslavfedis <rostyk110501@gmail.com>
2020-12-08 23:30:56 +0200

    homework-3 done

--- src/App.js
+++ src/App.js
@@ -2,33 +2,17 @@ import React, {Component} from 'react';
 import './comments/ComentStyle.css';
 import './posts/PostComponent.css';
 import './App.css';
-import AllComments from "./comments/AllComments";
 import AllPost from "./posts/AllPost";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import AllComments from "./comments/AllComments";
+
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div>
-
-                        <Link to={'/comments'}>comments</Link>
-
-
-                        <Link to={'/posts'}>posts</Link>
-
-                    <Switch>
-                        <Route path={'/comments'} component={AllComments}></Route>
-                        <Route path={'/posts'} component={AllPost}></Route>
-                    </Switch>
-
-                </div>
-            </Router>
+            <div>
+                <AllPost/>
+                <AllComments/>
+            </div>
         );
     }
 }
