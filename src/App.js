0fe5cd68455511638bfb5ad6bcec88fbddf7b953
rostyslavfedis <rostyk110501@gmail.com>
2020-12-25 18:10:53 +0200

    homework-7 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React, {Component} from 'react';
+import React, {memo, useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import './App.css'
 
-class App extends Component {
-  render() {
+const Test = memo(() => {
+    console.log('test was rendered');
+    const [user, setUser] = useState()
+    const storeCounter = useSelector(({counter}) => counter);
+    const dispatch = useDispatch();
+    const up = () => dispatch({type: 'INC_COUNTER'});
+    const down = () => dispatch({type: 'DEC_COUNTER'});
+    const reset = () => dispatch({type: 'RESET_COUNTER'});
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${storeCounter}`)
+            .then(response => response.json())
+            .then(json => setUser(json))
+    },[storeCounter])
     return (
-        <div>
-          
+        <div className={'Main'}>
+        <div className={'Counter'}>
+            <h2>COUNTER: {storeCounter}</h2>
+            <button className={'UP'} onClick={up}>UP</button>
+            <button className={'DOWN'} onClick={down}>DOWN</button>
+            <button className={'RESET'} onClick={reset}>RESET</button>
         </div>
-    );
-  }
-}
 
-export default App;
+            <div className="Users">
+                <h2>USERS FROM API</h2>
+
+                {!!user &&(
+                    <>
+                        <h3>id: {user.id}</h3>
+                        <h3>name: {user.name}</h3>
+                        <h3>username: {user.username}</h3>
+                        <h3>phone: {user.phone}</h3>
+                        <h3>website: {user.website}</h3>
+                    </>
+                )
+                }
+            </div>
+
+        </div>
+    )
+})
+
+
+export default function App() {
+    return (
+        <div className="App">
+
+            <Test title={"test"}></Test>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const initialState = {
+    counter: 1
+}
+
+const reduser = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER': {
+            return {...state, counter: state.counter + 1}
+
+        }
+        case 'DEC_COUNTER': {
+            return {...state, counter: state.counter - 1}
+
+        }
+        case 'RESET_COUNTER': {
+            return {...state, counter: 1}
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+export const store = createStore(reduser)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+        <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
