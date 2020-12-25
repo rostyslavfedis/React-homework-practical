2d600103feec077d69bee0994d3beb2c2620ef7f
rostyslavfedis <rostyk110501@gmail.com>
2020-12-25 13:39:11 +0200

    homework-6 done 85%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
+import './App.css';
 
-class App extends Component {
-  render() {
+export default function App() {
+    const [counter, setCounter] = useState(1)
+    const [user, setUser] = useState()
+
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${counter}`)
+            .then(response => response.json())
+            .then(json => setUser(json))
+    },[counter])
+
+    const increment = () => {
+        setCounter(prevState=>prevState + 1);
+    }
+    const decrement = () => {
+        setCounter(prevState=>prevState - 1);
+    }
+    const reset=()=>{
+        setCounter(1)
+    }
     return (
-        <div>
-          
-        </div>
-    );
-  }
-}
+        <div className="App">
 
-export default App;
+            <div className="Counter">
+                <h2>Counter value: {counter}</h2>
+                <button className={"UP"} onClick={increment}>UP</button>
+            <button className={"DOWN"} onClick={decrement}>DOWN</button>
+            <button className={"RESET"} onClick={reset}>RESET</button>
+            </div>
+
+            <div className="Users">
+                <h2>USERS FROM API</h2>
+
+                {!!user &&(
+                    <>
+                    <h3>id: {user.id}</h3>
+                    <h3>name: {user.name}</h3>
+                    <h3>username: {user.username}</h3>
+                    <h3>phone: {user.phone}</h3>
+                    <h3>website: {user.website}</h3>
+                    </>
+                    )
+                }
+            </div>
+        </div>
+    )
+
+
+}
